77c4fa1adce1f65749d0fdd5978f3fdabb804f01
rpadalka <moxnoryl@tut.by>
2016-11-09 23:39:41 +0300

    Initial push Add entities and configure Entity Manager

--- spring-mvc-application/src/main/java/by/home/entity/User.java
+++ spring-mvc-application/src/main/java/by/home/entity/User.java
@@ -1,14 +1,18 @@
 package by.home.entity;
 
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.persistence.Entity;
+
 /**
  * Created by rpadalka on 11/9/2016.
  */
-public class User {
+/*public class User {
 
-}
+}*/
 
-/*@Entity
+@Entity
 public class User extends AbstractPersistable<Long> {
 
     private static final long serialVersionUID = 1303417131998025391L;
-}*/
+}
